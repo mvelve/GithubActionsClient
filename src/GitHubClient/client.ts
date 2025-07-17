93c6a502f40844bdab37f721e0c8fe8174be0154
mvelve <mcvelve@gmail.com>
2025-07-17 06:31:50 -0700

    <WIP> refactored for controllability working on CLI

--- src/GitHubClient/client.ts
+++ src/GitHubClient/client.ts
@@ -2,15 +2,58 @@
 
 //load environment variables
 import dotenv from "dotenv";
-import octokit, { Octokit } from "octokit";
+import { Octokit } from "octokit"; //may need octokit wait on this
+import Answer from "../IAnswer";
 dotenv.config();
 
 export default class GithubClient {
+  private userAnswer: Answer;
   private octokitClient: Octokit;
 
-  constructor() {
-    this.octokitClient = new Octokit({ auth: process.env.AUTH });
+  constructor(userAnswer: Answer) {
+    this.userAnswer = userAnswer;
+    this.octokitClient = new Octokit({ auth: process.env.GITHUB_API_TOKEN });
   }
+
+  //isolate functionality within this class
+  async requestActionWorkflow() {
+    try {
+      await this.octokitClient.repos.getContent({
+        owner: this.userAnswer.repoOwner,
+        repo: this.userAnswer.repoName,
+        path: `${this.userAnswer.workFlowFileName}.yml`,
+      });
+    } catch (err: any) {
+      if (err.status === 404) {
+        console.log("Workflow file not found.");
+      } else {
+        console.error("GitHub API error:", err.status, err.message);
+      }
+
+      //now you need something in here so that you can create it
+    }
+  }
+
+  /*
+await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
+            owner: owner,
+            repo: "",
+            path: ".github/workflows/my-workflow.yml",
+            message,
+            committer: {
+                name:owner,
+                email:"assoulsidali@gmail.com",
+            },
+            content,
+            headers: {
+              'X-GitHub-Api-Version': '2022-11-28'
+            }
+          })
+          .catch(error => {
+            console.error('Error creating workflow file:', error);
+          });
+
+  */
 
   initializeWebhook() {
     console.log("hello world");
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,62 @@
 "use strict";
 import GithubClient from "./GitHubClient/client";
+import readline from "node:readline";
+import dotenv from "dotenv";
+import Answer from "./IAnswer";
 
-const reqClient = new GithubClient();
-reqClient.initializeWebhook();
+dotenv.config();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const checkEnvFiles = function () {
+  const unsetEnvVars: string[] = [];
+  for (const [key, val] of Object.entries(process.env)) {
+    if (!val) {
+      unsetEnvVars.push(key);
+    }
+  }
+
+  return unsetEnvVars.length === 0
+    ? ""
+    : `The following env vars are not set ${unsetEnvVars.join(", ")}`;
+};
+
+//es6 way ts may autocast async if just returned from async func
+const askQuestionAsync = function (questionText: string): Promise<string> {
+  return new Promise((resolve, _) =>
+    rl.question(questionText, (answer) => resolve(answer.trim()))
+  );
+};
+
+const askQuestions = async function (): Promise<Answer> {
+  let repoOwner, repoName, workFlowFileName;
+
+  while (!repoName || !repoOwner || !workFlowFileName) {
+    repoOwner = await askQuestionAsync("Owner Name: ");
+    repoName = await askQuestionAsync("Repository Name: ");
+    workFlowFileName = await askQuestionAsync("YAML Workflow Name: ");
+  }
+
+  //close the readline interface and return baseUrl to caller
+  rl.close();
+
+  return {
+    repoOwner: repoOwner,
+    repoName: repoName,
+    workFlowFileName: workFlowFileName,
+  };
+};
+
+const startClientWorkflow = async function () {
+  const missingFiles: string = checkEnvFiles();
+  if (missingFiles) {
+    console.log(missingFiles);
+    return; //maybe start the workflow again just check here
+  }
+
+  const answer: Answer = await askQuestions();
+  const client = new GithubClient(answer);
+};
